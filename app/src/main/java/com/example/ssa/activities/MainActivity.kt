37c9a819732d5821a27de5cee6685b1e68e00f82
Arrington Perry <perryceo21@hotmail.com>
2024-01-15 22:31:02 -0500

    main login screen design complete an navgraph implementation started

--- app/src/main/java/com/example/ssa/activities/MainActivity.kt
+++ app/src/main/java/com/example/ssa/activities/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.ssa.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.ssa.R
 import com.example.ssa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bind: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
 }
