8d485f8d119155224390d0a831767185188db0d7
Arrington Perry <perryceo21@hotmail.com>
2024-01-17 16:41:28 -0500

    log in pop up dialog design complete and button click events set with navigation.

--- app/src/main/java/com/example/ssa/activities/MainActivity.kt
+++ app/src/main/java/com/example/ssa/activities/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.ssa.activities
 
+import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -10,6 +13,7 @@ import androidx.navigation.Navigation
 import com.example.ssa.R
 import com.example.ssa.databinding.ActivityMainBinding
 import com.example.ssa.main_ui.MainFragment
+import com.example.ssa.main_ui.RegisterFragment
 import com.example.ssa.utilites.Navigator
 
 class MainActivity : AppCompatActivity(), Navigator {
@@ -21,8 +25,30 @@ class MainActivity : AppCompatActivity(), Navigator {
         bind = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
 
+        //skip past user login + register to access the application
         bind.skipLoginText.setOnClickListener{
             fragNavigation(MainFragment(), false)
+        }
+
+        //initiate log in pop up dialog and check authorization
+        bind.loginButton.setOnClickListener {
+          bind.loginPopUp.logInDialog.visibility = View.VISIBLE
+            TODO("Set on touch listener to check for outside touch to close the popup")
+            TODO("Inside touch listener block add logic for user interaction of the submit+login button")
+        }
+
+        //switch to register screen
+        bind.registerButton.setOnClickListener {
+            fragNavigation(RegisterFragment(), false)
+        }
+
+        //google log in logic and screen switch
+        bind.googleSignIn.setOnClickListener {
+            TODO(" Add logic for google sign in...integrate google OAuth")
+        }
+        bind.adminLogin.setOnClickListener {
+            val intent = Intent(this, AgentAdminActivity::class.java)
+            startActivity(intent)
         }
 
     }
